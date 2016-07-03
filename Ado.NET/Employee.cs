75089e5dd0c5d092fddeb95c69c447bb18ff9f0c
Vladyslav Mykhaylyuk <vladyslaff87@gmail.com>
2016-07-03 20:51:05 +0300

    Added Ado.NET project. Work with a database.

---
+++ Ado.NET/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Ado.NET
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string Email { get; set; }
+        public string Salary { get; set; }
+
+
+        public Employee(int dbID, string dbName, DateTime dbBirthDay, string dbEmail, string dbSalary)
+        {
+            ID = dbID;
+            Name = dbName;
+            BirthDay = dbBirthDay;
+            Email = dbEmail;
+            Salary = dbSalary;
+        }
+
+        public override string ToString()
+        {
+            string employeeInfo = "Employee: " + ID + "; " + Name;
+            if (BirthDay != null)
+            {
+                employeeInfo += "; " + BirthDay;
+            }
+
+            if (Email != "")
+            {
+                employeeInfo += "; " + Email;
+            }
+
+            if (Salary != "")
+            {
+                employeeInfo += "; " + Salary;
+            }
+
+            employeeInfo += Environment.NewLine;
+
+            return employeeInfo;
+        }
+    }
+}
---
+++ Ado.NET/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadFromBase();
+            Console.ReadKey();
+        }
+
+        private static void ReadFromBase()
+        {
+            var conString = ConfigurationManager.AppSettings["ConnectionString"]; /* прочитали строку подключения */
+            /* читаем из файла конфигурации значение ключа - appsettings из App.config файла */
+
+            using (var connection = new SqlConnection(conString))
+            {
+                string commandText = @"SELECT DISTINCT 
+                                        e.Id, e.Name, e.Birthday, e.Email, e.Salary
+                                     FROM
+                                        Employees e
+                                     WHERE
+                                        LEN(e.Name) > @len";
+                var command = new SqlCommand(commandText, connection);
+                var parameter = new SqlParameter("@len", SqlDbType.Int);
+                parameter.Value = 20;
+                command.Parameters.Add(parameter);
+
+                connection.Open();
+
+                using (var reader = command.ExecuteReader())
+                    /* Здксь - Получить данные. Есть и другие методы для других целей */
+                {
+                    List<Employee> dataFromDb = new List<Employee>(); /* dynamic - единственный динамичекий тип в C# */
+                    while (reader.Read())
+                    {
+                        int Id = int.Parse(reader[0].ToString());
+                        string Name = reader[1].ToString();
+                        DateTime BirthDay = reader[2].GetType() != typeof (DBNull) /* DBNull = Database NULL */
+                            ? Convert.ToDateTime(reader[2])
+                            : new DateTime();
+                        string Email = reader[3].ToString();
+                        string Salary = reader[4].ToString();
+                        
+                        Employee item = new Employee(Id, Name, BirthDay, Email, Salary);
+
+                        dataFromDb.Add(item);
+                    }
+
+                    foreach (var employee in dataFromDb)
+                    {
+                        Console.Write(employee.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
